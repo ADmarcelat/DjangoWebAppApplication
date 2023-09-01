ef67336b21acfb5537b6bfebc230e68d54ed83bb
Adrien MARCELAT <marcelat.adrien@gmail.com>
2023-09-01 18:17:18 +0200

    Add details pages when clicking on movies name (Raise 404 error if id movie not existing)

--- movies/urls.py
+++ movies/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 # movies/
 
+app_name = 'movies'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:movie_id>', views.detail, name='detail')
 ]
